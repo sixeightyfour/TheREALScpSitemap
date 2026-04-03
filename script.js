54801de7bdf29b2a46067a25b09c44e8e1445318
sixeightyfour <scpthrowawayfour@proton.me>
2026-04-03 19:15:30 -0400

    Update script.js

--- script.js
+++ script.js
@@ -50,7 +50,7 @@ function onEachFeature(feature, layer) {
 }
 
 
-fetch("./map.geojson")
+fetch("./map (2).geojson")
   .then((response) => {
     if (!response.ok) {
       throw new Error(`Failed to load GeoJSON: ${response.status}`);
