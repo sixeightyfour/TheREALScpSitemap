0039ee1e3954ff96ecc56096e46f5bc4bcc017fc
sixeightyfour <scpthrowawayfour@proton.me>
2026-04-03 16:26:30 -0400

    Update script.js

--- script.js
+++ script.js
@@ -23,3 +23,18 @@ L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
   attribution:
     '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 }).addTo(map);
+
+function onEachFeature(feature, layer) {
+  layer.bindPopup(feature.properties.nazwa);
+}
+
+// adding geojson by fetch
+// of course you can use jquery, axios etc.
+fetch("/map.geojson")
+  .then((response) => response.json())
+  .then((data) => {
+    // use geoJSON
+    L.geoJSON(data, {
+      onEachFeature: onEachFeature,
+    }).addTo(map);
+  });
