072b62543fc6d0b32b9b08813ad533e0ea13b709
sixeightyfour <scpthrowawayfour@proton.me>
2026-04-05 02:37:38 -0400

    Update script.js

--- script.js
+++ script.js
@@ -56,7 +56,7 @@ function makePopupHtml(feature) {
 }
 
 
-fetch("./map (11).geojson")
+fetch("./map (13).geojson")
   .then((response) => {
     if (!response.ok) {
       throw new Error(`Failed to load GeoJSON: ${response.status}`);
