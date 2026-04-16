6fbb6e9eaf594a39803c46c140db3c781e4cb72c
sixeightyfour <scpthrowawayfour@proton.me>
2026-04-16 16:54:03 -0400

    Update script.js

--- script.js
+++ script.js
@@ -261,7 +261,7 @@ searchForm.addEventListener("submit", async (event) => {
   }
 });
 
-fetch("./ColorMap7.geojson")
+fetch("./ColorMap8.geojson")
   .then((response) => {
     if (!response.ok) {
       throw new Error(`Failed to load GeoJSON: ${response.status}`);
