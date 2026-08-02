ea233ba7f4d29a5eef50230e1ae88e93f71ce5d0
Danjon56 <scpthrowawayfour@proton.me>
2026-08-02 18:30:08 -0400

    Update script.js

--- script.js
+++ script.js
@@ -261,7 +261,7 @@ searchForm.addEventListener("submit", async (event) => {
   }
 });
 
-fetch("./ColorMap16.geojson")
+fetch("./ColorMap17.geojson")
   .then((response) => {
     if (!response.ok) {
       throw new Error(`Failed to load GeoJSON: ${response.status}`);
